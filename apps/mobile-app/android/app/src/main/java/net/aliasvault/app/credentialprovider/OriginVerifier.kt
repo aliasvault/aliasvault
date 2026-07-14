cf269c49a1f7ce5e62d81f2a1e4bdc968206f87b
Leendert de Borst <ldeborst@xivisoft.com>
2026-07-14 18:15:18 +0200

    Update OriginVerifier.kt

--- apps/mobile-app/android/app/src/main/java/net/aliasvault/app/credentialprovider/OriginVerifier.kt
+++ apps/mobile-app/android/app/src/main/java/net/aliasvault/app/credentialprovider/OriginVerifier.kt
@@ -35,7 +35,7 @@ class OriginVerifier {
          */
         // BEGIN_GENERATED: passkeys-allowlist
         // Source: https://www.gstatic.com/gpm-passkeys-privileged-apps/apps.json
-        // Last refreshed: 2026-07-02
+        // Last refreshed: 2026-07-14
         private val PRIVILEGED_ALLOWLIST_JSON = """
         {
           "apps": [
@@ -939,6 +939,30 @@ class OriginVerifier {
                   {
                     "build": "release",
                     "cert_fingerprint_sha256": "29:39:99:7A:2D:8F:07:30:3C:EB:37:AD:68:10:AF:EF:0B:DA:71:0B:E2:11:64:76:E3:52:5A:73:79:EC:2E:1A"
+                  }
+                ]
+              }
+            },
+            {
+              "type": "android",
+              "info": {
+                "package_name": "com.opera.browser",
+                "signatures": [
+                  {
+                    "build": "release",
+                    "cert_fingerprint_sha256": "5D:6A:FB:F8:7F:65:2A:F0:46:47:AD:A0:DF:63:4C:F2:23:70:90:0B:16:4B:09:D5:0B:D2:3A:A2:CB:52:85:B8"
+                  }
+                ]
+              }
+            },
+            {
+              "type": "android",
+              "info": {
+                "package_name": "com.opera.browser.beta",
+                "signatures": [
+                  {
+                    "build": "release",
+                    "cert_fingerprint_sha256": "5D:6A:FB:F8:7F:65:2A:F0:46:47:AD:A0:DF:63:4C:F2:23:70:90:0B:16:4B:09:D5:0B:D2:3A:A2:CB:52:85:B8"
                   }
                 ]
               }
