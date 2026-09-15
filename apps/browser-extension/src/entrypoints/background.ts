5c1a246ad8aedcd2a7a307d79972d8f7dff44be9
Leendert de Borst <ldeborst@xivisoft.com>
2026-09-15 13:45:57 +0200

    Add background worker ping check and increase actual vault get timeout (#2407)

--- apps/browser-extension/src/entrypoints/background.ts
+++ apps/browser-extension/src/entrypoints/background.ts
@@ -211,7 +211,10 @@ export default defineBackground({
       }
     });
 
-    // Listen for messages via @webext-core/messaging
+    /*
+     * Listen for messages via @webext-core/messaging.
+     */
+    onMessage('PING', () => true);
     onMessage('CHECK_AUTH_STATUS', () => handleCheckAuthStatus());
 
     onMessage('GET_ENCRYPTION_KEY', () => handleGetEncryptionKey());
